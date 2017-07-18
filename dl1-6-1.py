5153e5a6261c4a52e8a17789c1e001f1347aefff
cutem-0104 <shuhei.sugaya@gmail.com>
2017-07-18 14:13:33 +0000

    コメント追加

--- dl1-6-1.py
+++ dl1-6-1.py
@@ -1,22 +1,26 @@
-#python 3.4.2で動作確認
+# python 3.4.2で動作確認
+# Dropboxへのアップロードを行うため、別途下記リンクを参考に設定が必要
+# https://auxin01.wordpress.com/2016/08/22/raspberry-pi_raspbian_dropbox-uploader2016/
 
+# 機械学習に必要なライブラリのインポート
 import numpy as np
 import matplotlib
 
 # matplotlib.pylotインポート前に
-# 記述するとsavefigがつかえる
+# 記述するとsavefig(グラフを画像ファイルに出力)がつかえる
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
-# ターミナルコマンド実行
+# ターミナルコマンド実行できるライブラリ
 import subprocess
 
-# ファイル名取得
+# オペレーティングインターフェース（ファイル名取得)
 import os.path
 
 # 日付を扱う
 from datetime import datetime, timedelta
 
+# スタックトレースの表示
 import traceback
 
 # データの作成
@@ -32,8 +36,8 @@ file_name = os.path.basename(__file__)
 # ファイル名と拡張子を分ける
 name, ext = os.path.splitext(file_name)
 
+# グラフをpngファイルで出力
 save_dir = "tmpFile/" + name + ".png"
-
 plt.savefig(save_dir)
 
 # 現在の時間を取得(日本時間に変換)
@@ -42,7 +46,6 @@ str_date = (datetime.now() + timedelta(hours=9)).strftime('%Y%m%d%H%M%S')
 try:
     # ホームディレクトリの取得
     homedir = os.getenv("HOME")
-    print(homedir)
 
     # pngファイルのパス取得
     file_dir =  homedir + "/Work/python/" + save_dir
